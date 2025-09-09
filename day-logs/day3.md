2b8feb867fa18c90c044a338c44f8d4d9b955a24
thangtrantoan <thangtrantoan.vn@gmail.com>
2025-09-09 10:52:37 +0700

    commit code day 3

--- day-logs/day3.md
+++ day-logs/day3.md
@@ -1,2 +1,10 @@
 ## Day 3 Log
-- 
+
+- Solve issues
+- Layout main
+
+* Identify the main color
+*
+
+- Menu
+- Unit test
--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,8 @@
-import React, { useState } from "react";
-import { generateShortId } from "./utils/generateId";
-import MainLayout from "./components/layout";
 import { RouterProvider } from "react-router-dom";
 import router from "./routers";
+import "./index.css";
 
 function App() {
-  const [newTask, setNewTask] = useState("");
-  const [tasks, setTasks] = useState(() => {
-    const saved = localStorage.getItem("tasks");
-    return saved ? JSON.parse(saved) : [];
-  });
-
-  React.useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks]);
-
-  const handleCreateTask = (e) => {
-    const data = {
-      id: generateShortId(),
-      status: "backlog", // completed
-      name: newTask,
-      timeCreate: Date.now(),
-    };
-    if (e.key === "Enter" && newTask.trim()) {
-      setTasks([data, ...tasks]);
-      setNewTask("");
-    }
-  };
-
-  const handleDeleteTask = (e) => {
-    const data = tasks.filter((i) => i.id !== e);
-    setTasks(data);
-  };
-
-  const handleChangeStatus = (id) => {
-    const updated = tasks.map((task) =>
-      task.id === id
-        ? {
-            ...task,
-            status: task.status === "completed" ? "backlog" : "completed",
-          }
-        : task
-    );
-    setTasks(updated);
-  };
-
   return (
     <div>
       <RouterProvider router={router} />
